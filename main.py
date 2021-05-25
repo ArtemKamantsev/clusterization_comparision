dfa84921500ade1a2b06eb57d4cf9ff1245ac767
Artem Kamantsev <kamantsev.artem@gmail.com>
2021-05-25 19:47:13 +0300

    feat: cbldp

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ from itertools import cycle, islice
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from scipy.optimize import linear_sum_assignment
 from sklearn import datasets
 from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
 
 from dbscan import dbscan
+from cbldp import cbldp
+
+DBSCAN = 'DBSCAN'
+CBLDP = 'CBLDP'
 
 
 def print_measures(labels_true, labels_predicted):
@@ -57,7 +62,7 @@ np.random.seed(0)
 
 n_samples = 1500
 noisy_circles = datasets.make_circles(n_samples=n_samples, factor=.6,
-                                      noise=.05)
+                                      noise=.1)
 noisy_moons = datasets.make_moons(n_samples=n_samples, noise=.05)
 blobs = datasets.make_blobs(n_samples=n_samples, random_state=8)
 no_structure = np.random.rand(n_samples, 2), np.zeros((n_samples,), dtype=int)
@@ -72,7 +77,13 @@ aniso = (X_aniso, y)
 # blobs with varied variances
 varied = datasets.make_blobs(n_samples=n_samples,
                              cluster_std=[1.0, 2.5, 0.5],
-                             random_state=random_state,)
+                             random_state=random_state, )
+
+compound_full = pd.read_csv('./datasets/Compound.txt', sep='	').to_numpy()
+compound = compound_full[:, :2], compound_full[:, 2]
+
+jain_full = pd.read_csv('./datasets/jain.txt', sep='	').to_numpy()
+jain = jain_full[:, :2], jain_full[:, 2]
 
 plt.figure(figsize=(9 * 2 + 3, 13))
 plt.subplots_adjust(left=.02, right=.98, bottom=.001, top=.95, wspace=.05,
@@ -81,31 +92,71 @@ plt.subplots_adjust(left=.02, right=.98, bottom=.001, top=.95, wspace=.05,
 plot_num = 1
 
 default_base = {
-    'eps': .3,
-    'min_samples': 20,
+    DBSCAN: {
+        'eps': .3,
+        'min_samples': 50,
+    },
+    CBLDP: {
+        'k': 40,
+        'minpts': 20,
+    }
 }
 
 datasets = [
-    ('Noisy circles', noisy_circles, {}),
-    ('Noisy moons', noisy_moons, {}),
-    ('Varied', varied, {'eps': .18, 'min_samples': 5}),
-    ('Anisotropy', aniso, {'eps': .15, 'min_samples': 20}),
-    ('Blobs', blobs, {}),
-    ('No structure', no_structure, {})
+    ('Noisy circles', noisy_circles, {
+        DBSCAN: {},
+        CBLDP: {},
+    }),
+    # ('Noisy moons', noisy_moons, {
+    #     DBSCAN: {},
+    #     CBLDP: {},
+    # }),
+    # ('Varied', varied, {
+    #     DBSCAN: {'eps': .18, 'min_samples': 5},
+    #     CBLDP: {},
+    #
+    # }),
+    # ('Anisotropy', aniso, {
+    #     DBSCAN: {'eps': .15, 'min_samples': 20},
+    #     CBLDP: { },
+    # }),
+    # ('Blobs', blobs, {
+    #     DBSCAN: {},
+    #     CBLDP: {},
+    # }),
+    # ('No structure', no_structure, {
+    #     DBSCAN: {},
+    #     CBLDP: {},
+    # })
+    ('Compound', compound, {
+        DBSCAN: {},
+        CBLDP: {
+            'k': 9,
+            'minpts': 4,
+        },
+    }),
+    ('Jain', jain, {
+        DBSCAN: {},
+        CBLDP: {
+            'k': 15,
+            'minpts': 4,
+        },
+    })
 ]
-clustering_algorithms = (
-    ('DBSCAN', dbscan),
-)
+clustering_algorithms = [
+    (DBSCAN, dbscan),
+    (CBLDP, cbldp)
+]
 
 for i_dataset, (title, dataset, algo_params) in enumerate(datasets):
     print(title)
-    params = default_base.copy()
-    params.update(algo_params)
 
     X, y = dataset
     X = StandardScaler().fit_transform(X)
 
     for name, algorithm in clustering_algorithms:
+        params = default_base[name].copy()
+        params.update(algo_params[name])
         t0 = time.perf_counter()
         y_pred = algorithm(X, **params)
         t1 = time.perf_counter()
